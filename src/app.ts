7c3fed1f2f2af7581ee851b46ac6d336099e1229
DariusPirvulescu <organicdarius@gmail.com>
2020-09-10 21:35:28 +0300

    Render template on btn click

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-// Code goes here!
+const btn = document.getElementById("load")
+
+
+function show() {
+  const template = document.querySelector("template")!
+  const clone = template.content.cloneNode(true)
+  const app = document.getElementById("app")!
+  app.appendChild(clone)
+  console.log("CLI")
+}
+
+btn?.addEventListener("click", function() {
+  show()
+})
+
+
