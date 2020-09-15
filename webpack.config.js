a9a64788cdd57c7d89c9b51797909c2354c3dd0a
DariusPirvulescu <organicdarius@gmail.com>
2020-09-15 19:00:21 +0300

    Optimize webpack.config.js for development, Add clean-webpack-plugin package

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/app.ts",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "dist"
   },
   devtool: "inline-source-map",
   module: {
@@ -15,5 +17,8 @@ module.exports = {
         exclude: /node_modules/
       }
     ]
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
   }
 }
